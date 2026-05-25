3b92502aceecf427979f2b1b657a207087ac59dc
DESKTOP-F6MCPVR\lilinfei <lixc@qll-times.com>
2026-05-25 23:19:22 +0800

    add

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,10 @@
-import express from 'express';
+import express,{ Request,Response,NextFunction,Errback } from 'express';
 export default function(app: express.Application){
   app.use(express.json());
+  app.use((err: any,req:Request,res:Response,next:NextFunction) => {
+    res.status(200).json({
+      status:1,
+      message:err.message
+    });
+  })
 }
--- src/router/booksType.ts
+++ src/router/booksType.ts
@@ -14,28 +14,59 @@ const typeSchema = new Schema<TypeSchema>({
   }
 });
 const TypeModel = mongoose.model('type',typeSchema);
-// const TypeModel = mongoose.model('type');
 const router:Router = express.Router();
+router.post("/delete",async (req:Request,res:Response) => { 
+  let _id = req.body._id;
+  if(!_id) return res.json({
+    status:1,
+    message:"_id不能为空"
+  });
+  try{
+    await TypeModel.deleteOne({_id});
+    res.json({
+      status:0,
+      message:"删除成功"
+    });
+  }catch(e){
+    console.log(e);
+  }
+})
 router.post("/add",async (req,res) => {
+  if(!req.body.name) return res.json({
+    status:1,
+    message:"name不能为空"
+  });
+  const booksType = new TypeModel<TypeSchema>({
+    name:req.body.name,
+    created_time:new Date()
+  });
   console.log(req.body)
-  res.json({
-    status:0,
-    data:"OK"
-  });
-  // const booksType = new TypeModel<TypeSchema>({
-  //   name:"1111",
-  //   created_time:new Date()
-  // });
-  // await booksType.save();
-  // res.json({
-  //   status:0,
-  //   data:"OK"
-  // });
+  try{
+    let createRes = await booksType.save();
+    console.log(createRes)
+    res.json({
+      status:0,
+      data:{
+        _id:createRes._id,
+        name:createRes.name,
+        created_time:createRes.created_time
+      },
+      message:"创建成功"
+    });
+  }catch(e){
+    console.log(e);
+  }
 });
-router.get("/list",(req:Request,res:Response) => {
-  res.json({
-    status:0,
-    data:[]
-  });
+router.get("/list",async (req:Request,res:Response) => {
+  let list: object[] = [];
+  try{
+    list = await TypeModel.find({},{__v:0}).sort({created_time:-1});
+    res.json({
+      status:0,
+      data:list
+    });
+  }catch(e){
+    console.log(e);
+  }
 });
 export default router;
