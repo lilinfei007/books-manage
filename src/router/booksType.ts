3ea6737954cd93967d92e65cbca9759a9326e8ea
DESKTOP-F6MCPVR\lilinfei <lixc@qll-times.com>
2026-05-26 23:55:09 +0800

    add

--- src/router/booksType.ts
+++ src/router/booksType.ts
@@ -1,4 +1,4 @@
-import express,{ Router,Request,Response } from 'express';
+import express,{ Router,Request,Response,NextFunction } from 'express';
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 interface TypeSchema {
@@ -13,8 +13,54 @@ const typeSchema = new Schema<TypeSchema>({
     default:Date.now
   }
 });
+
+
+interface Validator {
+  required:boolean;
+  type:"string" | "number" | "object" | "boolean" | "array" | "date" | "buffer" | "mixed" | "objectid" | "string[]" | "number[]" | "object[]" | "boolean[]" | "array[]" | "date[]" | "buffer[]" | "mixed[]" | "objectid[]" | "any[]" | "any" | "string|number" | "string|object" | "string|boolean" | "string|array" | "string|date";
+}
+
+interface ModifyValidator extends Record<string, Validator> {
+  _id:Validator;
+  name:Validator;
+}
+
+const validatorHandle = <T extends Record<string,Validator>>(validtor: T) => {
+  return (req:Request,res:Response,next:NextFunction) => {
+    for(const [key,value] of Object.entries(validtor)){
+      if(value.required){
+        if(!req.body[key]){
+          return res.json({
+            status:1,
+            message:`${key}不能为空`
+          });
+        }
+      }
+    }
+    next();
+  }
+}
+const modifyValidator:ModifyValidator = {
+  _id:{
+    required:true,
+    type:"string"
+  },
+  name:{
+    required:true,
+    type:"string"
+  }
+}
 const TypeModel = mongoose.model('type',typeSchema);
 const router:Router = express.Router();
+router.put("/modify",validatorHandle(modifyValidator),async (req:Request,res:Response) => {
+  let _id = req.body._id;
+  let name = req.body.name;
+  try{
+    await TypeModel.updateOne({_id},{name});
+  }catch(e){
+    console.log(e);
+  }
+})
 router.post("/delete",async (req:Request,res:Response) => { 
   let _id = req.body._id;
   if(!_id) return res.json({
